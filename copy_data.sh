8738be9e6f460499b2e3b89a892fbc6c56d8aef1
Zhicai <zzhang2@caltech.edu>
2019-03-02 16:15:51 -0800

    update

--- copy_data.sh
+++ copy_data.sh
@@ -1,2 +1,4 @@
 mkdir -p data
-wget http://zhicaiz.web.cern.ch/zhicaiz/sharebox/data/Pi0Net/photonNtuple.root ./data
+cd data
+wget http://zhicaiz.web.cern.ch/zhicaiz/sharebox/data/Pi0Net/photonNtuple.root 
+cd -
